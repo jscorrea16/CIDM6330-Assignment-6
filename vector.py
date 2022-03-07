b9636178cb5f0a4f4dfea0a07e51dcbd5de3da0b
Joshua Correa <jscorrea1@buffs.wtamu.edu>
2022-03-06 18:36:49 -0600

    Vector.py

---
+++ vector.py
@@ -0,0 +1,15 @@
+class Arthropod:
+    def __init__(type, insect, arachnid):
+        type.insect = insect
+        type.arachnid = arachnid
+
+
+class Geographical:
+    def __init__(location, county, city):
+        location.county = county
+        location.city = city
+
+
+class Glossary:
+    def __init__(definition, culicidae):
+        definition.culicidae = culicidae
